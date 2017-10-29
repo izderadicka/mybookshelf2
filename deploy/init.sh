d2632e21e4425e25f4b0f567eb5be520bac1cd7b
Ivan <ivan.zderadicka@gmail.com>
2017-10-29 08:07:41 +0100

    development deployment working

--- deploy/init.sh
+++ deploy/init.sh
@@ -17,42 +17,47 @@ fi
 }
 
 if [[ -z "$1" ]]; then 
-echo Usage: $0 [development|stage]
-exit 1
+    echo Usage: $0 [development|stage]
+    exit 1
 fi
-
+CURRENT_USER=$(id -u):$(id -g)
 read_password "Database password: "  MBS2_DB_PASSWORD
 
 if [[ "$1" = "development" ]]; then
-cat <<EOF >.env
+    cat <<EOF >.env
 POSTGRES_PASSWORD="$MBS2_DB_PASSWORD"
 MBS2_ENVIRONMENT=development
 MBS2_DEBUG=true
 CODE_VOLUME=..
+MBS2_USER=$CURRENT_USER
+MBS2_USER_NAME=$USER
 EOF
-
-
 elif [[ "$1" = "stage" ]]; then
-echo "Stage environment"
-exit 1
+    cat <<EOF >.env
+POSTGRES_PASSWORD="$MBS2_DB_PASSWORD"
+MBS2_ENVIRONMENT=development
+MBS2_DEBUG=true
+CODE_VOLUME=..
+MBS2_USER=$CURRENT_USER
+
+EOF
 else
-echo Uknown environment
-exit 1
+    echo Uknown environment
+    exit 1
 fi
-export CURRENT_USER=$(id -u):$(id -g)
+export $(cat .env | xargs)
 docker build -t mbs2-ubuntu .
 read_password "Mybookshelf2 admin password: " MBS2_ADMIN_PASSWORD
 docker-compose up -d db
 sleep 3
-docker-compose run --user $CURRENT_USER --rm  app python3 manage.py create_tables -a -c
+docker-compose run --user $MBS2_USER --rm  app python3 manage.py create_tables -a -c
 docker-compose run --rm  app python3 manage.py change_password admin -p "$MBS2_ADMIN_PASSWORD"
-echo 
 
 CLIENT_IMAGE=mbs2-client-build-image
 docker build -t $CLIENT_IMAGE -f Dockerfile-build-client .
 if [[ "$1" = "development" ]]; then
 
-cat <<EOF
+    cat <<EOF
 #####################################################
 Now MyBookself2 is running in developement mode
 serving code from local directory.
@@ -63,7 +68,9 @@ Both client code and server code (apart of backend)
 are in watch mode - so any changes are applied immediatelly
 ######################################################
 EOF
-docker-compose up
+    docker-compose up
+    echo
+    echo You can restart application with running docker-compose up from this directory
 fi
 
 
